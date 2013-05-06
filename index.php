381d3b844879b241f0cb7a2eb98b874b7ac062fd
wok <wok@foodshelterlove.com>
2013-05-06 12:23:14 -0700

    add some basic markup

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php phpinfo(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+
+
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+	
+</body>
+</html>
+
+<?php ?>
